f572af5b4fad8a01a47755fb0e1b2c3d7865aae5
afrasiab <afrasiabamambay@gmail.com>
2026-04-28 15:42:27 -0400

    implemented a matrix addition function for specifically adding bias

--- matrix.h
+++ matrix.h
@@ -87,6 +87,16 @@ static inline void matrix_add(matrix* result, const matrix* a, const matrix* b)
 		result->_data[i] = a->_data[i] + b->_data[i];
 }
 
+static inline void matrix_add_bias(matrix* result, const matrix* bias)
+{
+	assert(result->_cols == bias->_cols);
+	assert(bias->_rows == 1); // each column will be a neurons bias for one row.
+
+	for (size_t r = 0; r < result->_rows; ++r)
+		for (size_t c = 0; c < result->_cols ++c)
+			MATRIX_AT(*result, c, r) += MATRIX_AT(*bias, c, 0);
+}
+
 static void matrix_rand(matrix* m) 
 {
 	size_t total_size = m->_cols * m->_rows;
