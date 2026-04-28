516ca650c9fa2656d7614ee78ae9af706fbf8d8a
afrasiab <afrasiabamambay@gmail.com>
2026-04-28 14:21:34 -0400

    implemented the sigmoid and softmax activation functions

--- activation.h
+++ activation.h
@@ -1,16 +1,37 @@
 #ifndef ACTIVATION_H
 #define ACTIVATION_H
 
+#include <math.h>
 #include "matrix.h"
 
-static inline matrix* sigmoid()
+static inline matrix* sigmoid(matrix* m)
 {
-
+	size_t total_size = m->_cols * m->_rows;
+	for (size_t i = 0; i < total_size; ++i)
+		m->_data[i] = 1.0f / (1.0f + exp(-m->_data[i]));
+	return m;
 }
 
-static inline matrix* softmax()
+static inline matrix* softmax(matrix* m)
 {
+	size_t total_size = m->_cols * m->_rows;
 
+	// Prevent huge values and breaking due to inf by subtracting every number by the maximum number to keep the result the same but the number smaller.
+	double max = m->_data[0];
+	for (size_t i = 1; i < total_size; ++i)
+		if (m->_data[i] > max)
+			max = m->_data[i];
+
+	double sum = 0.0f;
+	for (size_t i = 0; i < total_size; ++i)
+	{
+		m->_data[i] = exp(m->_data[i] - max);
+		sum += m->_data[i];
+	}
+	for (size_t i = 0; i < total_size; ++i)
+		m->_data[i] /= sum;
+
+	return m;
 }
 
 #endif // !ACTIVATION_H
